9271c942f4311e01139523ff090875de488aa8fa
Justin Deutsch <djustin8@vt.edu>
2021-04-12 13:14:13 -0400

    added basic nvim settings

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -5,3 +5,16 @@ vim.o.guifont = "JetBrainsMono\\ Nerd\\ Font\\ Mono:h18"
 vim.wo.number = true
 
 vim.o.t_Co = "256"
+
+-- LINE SETTINGS
+vim.wo.wrap = false
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.cursorline = true
+
+-- always show sign column
+vim.wo.signcolumn = "yes"
+
+-- SANE SPLITTING DIRECTIONS
+vim.o.splitbelow = true
+vim.o.splitright = true
