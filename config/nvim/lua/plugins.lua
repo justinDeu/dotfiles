4c7654fc4348f3337da711777d5dfbcf62f14f01
Justin Deutsch <djustin8@vt.edu>
2023-02-02 22:17:20 -0500

    add nvim-tree

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -48,6 +48,13 @@ return require('packer').startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
 
-  use("folke/zen-mode.nvim")
+  use {
+  'nvim-tree/nvim-tree.lua',
+  requires = {
+    'nvim-tree/nvim-web-devicons', -- optional, for file icons
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
 
+  use("folke/zen-mode.nvim")
 end)
--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -5,4 +5,7 @@ if [ "$SELECTED" = "true" ]; then
   WIDTH="0"
 fi
 
+# Hack to not have them show icons
+WIDTH="0"
+
 sketchybar --animate tanh 20 --set $NAME icon.highlight=$SELECTED label.width=$WIDTH
