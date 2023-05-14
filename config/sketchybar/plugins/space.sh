da702a9e643141df9da727817a8df2d7da1bf8a2
Justin Deutsch <djustin8@vt.edu>
2023-05-14 17:00:31 -0400

    add more spaces to sketchybar:

--- config/sketchybar/plugins/space.sh
+++ config/sketchybar/plugins/space.sh
@@ -2,10 +2,7 @@
 
 WIDTH="dynamic"
 if [ "$SELECTED" = "true" ]; then
-  WIDTH="0"
+    WIDTH="0"
 fi
-
-# Hack to not have them show icons
-WIDTH="0"
 
 sketchybar --animate tanh 20 --set $NAME icon.highlight=$SELECTED label.width=$WIDTH
